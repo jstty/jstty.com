0eaa7f2d31ab80c24cfdb4b060efd14f6cfc803d
Joseph Sutton <joe@jstty.com>
2016-01-09 18:23:53 -0800

    removed old files, starting from new!

--- client/app/scripts/controllers/portfolio.js
+++
@@ -1,8 +0,0 @@
-define(['angular'], function (angular) {
-  'use strict';
-
-  angular.module('app.controllers.PortfolioCtrl', [])
-    .controller('PortfolioCtrl', function ($scope) {
-
-    });
-});
