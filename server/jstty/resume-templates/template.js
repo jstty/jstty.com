e0a85ee715bf3da7b88d29b564dd5dfdb345ab89
jstty <joe@jstty.com>
2017-11-16 01:24:50 -0800

    (fix) phone text

--- server/jstty/resume-templates/template.js
+++ server/jstty/resume-templates/template.js
@@ -15,7 +15,7 @@ var header = {
 var contact = {
     width: '100%',
     table: {
-        widths: [26, 1, 'auto', '*', 36, 2, 'auto'],
+        widths: [27, 1, 'auto', '*', 36, 2, 'auto'],
         body: [
             ['phone', ':', '<%= phone %>'         ,'', 'webfolio', ':', { text: '<%= webfolio %>'} ],
             ['email', ':', { text:'<%= email %>'} ,'', 'github', ':', {text: '<%= github %>'} ]
